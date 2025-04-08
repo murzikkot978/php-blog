fa27275009408ea6f670b82ae452349e98a6cb70
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-04-08 15:44:06 +0200

    feat: all pages are added and the user can view them

---
+++ public/login.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="compiled.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php require "navigationBar.php" ?>
+
+<div class="flex flex-col gap-[20px] justify-center items-center w-full h-full ">
+    <div class="flex flex-col w-[100%] h-[100%] justify-center items-center">
+        <p class="text-[60px] w-[300px] flex justify-center">Log in</p><br>
+        <form class="custom-form">
+
+            <label for="name" class="text-[20px]">Write your name</label>
+            <input name="name" type="text" class="border-[2px]"><br>
+
+            <label for="email" class="text-[20px]">Write email</label>
+            <input name="email" type="email" class="border-[2px]"><br>
+
+            <label for="pasword" class="text-[20px]">Write pasword</label>
+            <input name="pasword" type="text" class="border-[2px]"><br>
+
+            <button type="submit" class="border-[2px] w-[100px]">Submit</button>
+            <div class="flex gap-[15px]">
+                <div class="flex gap-[7px]">
+                    <p>No account ? </p>
+                    <a href="register.php" class="text-[red]">Create one</a>
+                </div>
+                <div class="flex gap-[5px]">
+                    <p>Back to - </p>
+                    <a href="index.php" class="text-[red]">Home page</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ public/navigationBar.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<header class="header">
+    <nav class="navbar">
+        <div>
+            <a href="index.php" class="logoHome"><img src="logos/logoHome.png"></a>
+        </div>
+        <div class="flex gap-[50px]">
+            <div class="flex gap-[20px]">
+                <a href="postCreation.php" class="flex text-[20px]">postCreation</a>
+            </div>
+            <div>
+                <a href="login.php" class="flex text-[20px]">log in</a>
+            </div>
+        </div>
+    </nav>
+</header>
